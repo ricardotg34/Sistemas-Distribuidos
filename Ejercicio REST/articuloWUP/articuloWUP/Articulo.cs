fef745e445c3c06a9e291aab8482df21908920fe
Mayrasho <37519446+Mayrasho@users.noreply.github.com>
2019-12-06 00:12:17 -0600

    UWP client added - create articulo implemented

---
+++ Ejercicio REST/articuloWUP/articuloWUP/Articulo.cs
@@ -0,0 +1,12 @@
+﻿// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace articuloWUP
+{
+    public class Articulo
+    {
+        public string claveArticulo { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; }
+        public int existencias { get; set; }
+    }
+}
---
+++ Ejercicio REST/articuloWUP/articuloWUP/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace articuloWUP
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Articulo a = new Articulo();
+            a.claveArticulo = claveArticuloTxt.Text;
+            a.descripcion = descripcionTxt.Text;
+            a.precio = Decimal.Parse(precioTxt.Text);
+            a.existencias = Int32.Parse(existenciasTxt.Text);
+            var json = JsonConvert.SerializeObject(a);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.PostAsync("http://127.0.0.1:8000/articulos/", data);
+                statusTxt.Text =  response.StatusCode.ToString();
+            }
+
+
+        }
+    }
+}
